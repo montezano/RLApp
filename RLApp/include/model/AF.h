bcb19039221db47af18b54d5976c83d925618333
Montezano <pablomontezano@gmail.com>
2017-11-10 09:01:00 -0200

    Add some documentation for FA

--- RLApp/include/model/AF.h
+++ RLApp/include/model/AF.h
@@ -15,23 +15,115 @@ public:
 	~FA();
 
 	//ACCESS FUNCTIONS
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Set the vector with terminals. Make a copy to the
+	///			existing QVector<VT>
+	///	\param	QVector<VT> set the terminals of the FA
+	///
+	////////////////////////////////////////////////////////////
 	void setTerminals(QVector<VT> terminals);
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Add another terminal to the terminal QVector<VT>
+	///	\param	VT terminal
+	///
+	////////////////////////////////////////////////////////////
 	void addTerminal(VT terminal);
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Get a copy of the QVector with terminals.
+	///	\return	QVector<VT> A copy of the QVector<VT> with all
+	///			terminals.
+	///
+	////////////////////////////////////////////////////////////
 	QVector<VT> getTerminals();
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Add a new state to the FA.
+	///	\param	QVector<TR> with the transitions of the state.
+	///			Transitions may be non deterministic (more than
+	///			transition for the same terminal)
+	///	\param	StateType	define if the state is FINAL, REGULAR,
+	///			INITIAL or a combination of those.
+	///	\return	bool if the operation were done successufully
+	///
+	////////////////////////////////////////////////////////////
 	bool addState(QVector<TR> transitions, StateType type);
 
-	//
+	////////////////////////////////////////////////////////////
+	/// \brief	Verify if the actual FA is empty. An empty FA is
+	///			one such that the initial state is dead.
+	///	\return	true if the actual FA is empty, false otherwise.
+	///
+	////////////////////////////////////////////////////////////
 	bool isEmpty();
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Verify if the actual FA is infinite of finite.
+	///			An infinite FA is one that generate sentences with
+	///			infite length.
+	///	\return	true if the actual FA is infinite, false otherwise.
+	///
+	////////////////////////////////////////////////////////////
 	bool isInfinite();
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Determinize the actual AF. The process consist
+	///			on removing the & transitions, and processing the
+	///			AF if it's an FDA, nothing will be done. If it's
+	///			a FNDA, it will be determinized.
+	///	\return	true if the process was completed successufully.
+	///			false otherwise.
+	///
+	////////////////////////////////////////////////////////////
 	bool determinize();
 
 	// Return true if it removed sucessufully
+	////////////////////////////////////////////////////////////
+	/// \brief	Remove the E transitions from the actual FA. This
+	///			method is necessary for the determinization algorithm.
+	///	\return	true if the process was completed successufully.
+	///			false otherwise.
+	///
+	////////////////////////////////////////////////////////////
 	bool removeETransition();
-	bool verify();
+
+	//////////////////////////////////////////////////////////////
+	///// \brief	Get the complete texture struct
+	/////	\param	ResourcesHandler::Texture	enum of the desired container
+	/////	\return	pointer to the desired container (DO NOT FREE THIS GUY!!!)
+	/////
+	//////////////////////////////////////////////////////////////
+	//bool verify();
 
 	// UTILS
+	////////////////////////////////////////////////////////////
+	/// \brief	Get the state name to be added next. Use this method
+	///			if you're adding a new state to the FA.
+	///	\return	unsigned the name of the next state name.
+	///
+	////////////////////////////////////////////////////////////
 	unsigned getNextStateName();
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Get a list with all final states from the current FA.
+	///	\return	QVector<NT> with all final states
+	///
+	////////////////////////////////////////////////////////////
 	QVector<NT> getFinalStates();
+
+	////////////////////////////////////////////////////////////
+	/// \brief	Get a list of all the states that makes the &
+	///			transition closure for a gien state. & transition
+	///			closure consist of all states reachable from the
+	///			& transition from the given state.
+	///	\param	NT		the name of the state to get the &
+	///					state closure.
+	///	\return	QVector<TR> the list of all states reachable from
+	///			& transition.
+	///
+	////////////////////////////////////////////////////////////
 	QVector<TR> getEStateClosure(NT state_name);
 
 private:
